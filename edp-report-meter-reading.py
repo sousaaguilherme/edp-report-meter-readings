149fdc7251ebec7aea9a7902281970e7e1722b91
sousaaguilherme <sousa.a.guilherme@gmail.com>
2021-10-08 11:09:08 +0100

    auth was only working if a browser was already logged in... had to add a few steps to the authentication. Seems to be working now

--- edp-report-meter-reading.py
+++ edp-report-meter-reading.py
@@ -5,13 +5,17 @@ import uuid
 from datetime import datetime
 import argparse
 
+
 edp_upload_reading_api_endpoint = 'https://edpzero.cliente.edp.pt/client/listeners/api/pt.edp.edponline.reading.uploadReading'
 
 edp_google_auth_api_key = 'AIzaSyBCyKe7wskhsEcIHK7C_FVQlJLlOBJPda8'
 
 edp_meter_date = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
 
-edp_payload_report_template = Template("""{
+edp_correlation_id=uuid.uuid4()
+
+edp_payload_report_template = Template("""
+{
    "correlationId":"${edp_correlation_id}",
    "args":{
       "contractId":"${edp_contract_id}",
@@ -69,25 +73,71 @@ edp_payload_report_template = Template("""{
    },
    "replacements":[
    ]
-}""")
+}
+""")
+    
+edp_payload_sso_template = Template("""
+{
+   "correlationId":"${correlationID}",
+   "args":{
+      "token":"${token}",
+      "channel":"browser"
+   },
+   "replacements":null,
+   "loading":false
+}
+""")
+
+google_verifyCustomToken_payload_template = Template("""
+{
+     "token":"${token}",
+     "returnSecureToken":true
+}                                                                                                          
+""")
+
+edp_payload_usersession_login_template = Template("""
+{
+     "correlationId":"${correlationID}",
+     "args":
+         {
+             "token_id":"${token}",
+             "platform":0
+         },
+         "replacements":[]
+}                                              
+""")
     
 def edp_sign_in(email, password):
-        request_ref = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key={0}".format(edp_google_auth_api_key)
+        ## Get authentication token from google idp
+        request_ref = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key={0}'.format(edp_google_auth_api_key)
         headers = {"content-type": "application/json; charset=UTF-8"}
         data = json.dumps({"email": email, "password": password, "returnSecureToken": True})
-        request_object = requests.post(request_ref, headers=headers, data=data)        
-        return request_object.json()
-    
+        request_object = requests.post(request_ref, headers=headers, data=data)
+        
+        ## Login to edp with authentication token provided on the previous step 
+        request_url = 'https://login.edp.pt/listeners/api/pt.edp.sso.auth.logIn'
+        resp = requests.post(request_url, edp_payload_sso_template.substitute(token=request_object.json()['idToken'], correlationID=edp_correlation_id), headers={'authorization': request_object.json()['idToken']})
+        firebase_token_cookie_header = resp.headers['set-cookie'].split(';')[3].split(',')[1].split('=')[1]
+        
+        ## Request authorization from google idp (i guess)
+        request_url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key={0}'.format(edp_google_auth_api_key)
+        resp = requests.post(request_url, google_verifyCustomToken_payload_template.substitute(token=firebase_token_cookie_header))
+        
+        ## Get user session token from edp with authentication and authorization tokens
+        request_url = 'https://edpzero.cliente.edp.pt/client/listeners/api/pt.edp.edponline.usersession.login'
+        resp = requests.post(request_url, edp_payload_usersession_login_template.substitute(token=resp.json()['idToken'], correlationID=edp_correlation_id), headers={'authorization': resp.json()['idToken']})
+        authorization_token_header = resp.headers['set-cookie'].split(';')[3].split(',')[1].split('=')[1]
+        
+        return firebase_token_cookie_header, authorization_token_header
 
 
 
-def submit_meter_reading(auth_token, edp_contract_id, edp_cpe_code, edp_serial_number, reading_vazio, reading_ponta, reading_cheia):
-    edp_payload_report = edp_payload_report_template.substitute(edp_correlation_id=uuid.uuid4(), edp_contract_id=edp_contract_id, edp_serial_number=edp_serial_number, edp_cpe_code=edp_cpe_code, edp_meter_date=edp_meter_date, edp_reading_vazio=reading_vazio, edp_reading_ponta=reading_ponta, edp_reading_cheia=reading_cheia)
-    resp = requests.post(edp_upload_reading_api_endpoint, edp_payload_report, headers={'Authorization': auth_token})
+def submit_meter_reading(firebase_token_cookie_header, authorization_token_header, edp_contract_id, edp_cpe_code, edp_serial_number, reading_vazio, reading_ponta, reading_cheia):
+    edp_payload_report = edp_payload_report_template.substitute(edp_correlation_id=edp_correlation_id, edp_contract_id=edp_contract_id, edp_serial_number=edp_serial_number, edp_cpe_code=edp_cpe_code, edp_meter_date=edp_meter_date, edp_reading_vazio=reading_vazio, edp_reading_ponta=reading_ponta, edp_reading_cheia=reading_cheia)
+    resp = requests.post(edp_upload_reading_api_endpoint, edp_payload_report, headers={'authorization': authorization_token_header, 'cookie': firebase_token_cookie_header})
     print(resp.text)
     
-    
-def main():
+def main():    
     arg_parser = argparse.ArgumentParser(prog='edp-report-meter-reading',
                                          usage='%(prog)s [args]',
                                          description='Report meter readings to edp',
@@ -121,12 +171,12 @@ def main():
 
 
     print("Authenticating ...")
-    auth = edp_sign_in(args.e, args.p)
-    auth_token = auth['idToken']
+    firebase_token_cookie_header, authorization_token_header = edp_sign_in(args.e, args.p)
     print("Uploading reading report to edp ...")
-    submit_meter_reading(auth_token, args.i, args.c, args.s, args.rv, args.rp, args.rc)
+    submit_meter_reading(firebase_token_cookie_header, authorization_token_header, args.i, args.c, args.s, args.rv, args.rp, args.rc)
 
 if __name__ == "__main__":
     main()
+
 
 
